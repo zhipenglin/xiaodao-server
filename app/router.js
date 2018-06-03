73610f84603b8034c0a7cbaded8e4811e0da9e6e
linzhipeng <zhipeng.lin@ifchange.com>
2018-06-03 13:01:05 +0800

    xxx

--- app/router.js
+++ app/router.js
@@ -6,9 +6,9 @@
 module.exports = app => {
     const {router, controller} = app;
     router.get('/', controller.home.index);
-    router.post('/login', controller.user.login);
+    router.post('/api/login', controller.user.login);
     router.post('/upload', controller.question.upload);
 
-    router.post('/questions',controller.game.getQuestions);
-    router.post('/record',controller.game.record);
+    router.post('/api/questions',controller.game.getQuestions);
+    router.post('/api/record',controller.game.record);
 };
